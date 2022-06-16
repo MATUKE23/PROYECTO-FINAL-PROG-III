0120fc92b99dde2dc3fe61425eaddbebec7ec80a
Ant1Dot0 <joselicla021@gmail.com>
2022-06-16 19:25:47 -0300

    lectura con cards hecha

---
+++ TpProgramacion3-Licla-Varela/Ecommerce/Productos.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace Ecommerce
+{
+    public partial class Productos : System.Web.UI.Page
+    {
+        public List<Articulo> ListaArticulos { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio aux = new ArticuloNegocio();
+            ListaArticulos = aux.listar();
+        }
+    }
+}
