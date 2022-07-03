965f465c6d44b0d2feb791cbe0e15a1733da774b
Ant1Dot0 <joselicla021@gmail.com>
2022-07-02 21:16:18 -0300

    Añadi page Producto y empiezo con el carrito de compra

--- TpProgramacion3-Licla-Varela/Ecommerce/Site1.Master.cs
+++ TpProgramacion3-Licla-Varela/Ecommerce/Site1.Master.cs
@@ -12,8 +12,17 @@ namespace Ecommerce
     public partial class Site1 : System.Web.UI.MasterPage
     {
         public List<Articulo> ListaArticulos { get; set; }
+        public List<Articulo> carrito { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            carrito = (List<Articulo>)Session["carritoCompra"];
+
+            if (carrito == null)
+            {
+                carrito = new List<Articulo>();
+                Session.Add("carritoCompra", carrito);
+            }
+
             ArticuloNegocio aux = new ArticuloNegocio();
             ListaArticulos = aux.listar();
         }
